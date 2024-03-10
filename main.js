9d8c0478abc57372d150b3b08ba7c14e7f67e7e9
SamehCode <jack_jones2010@hotmail.com>
2024-03-10 05:48:50 +0200

    uploaded main project files

--- main.js
+++ main.js
@@ -1,210 +1,227 @@
-const quizArea = document.querySelector('.quiz-area');
-const quizAnswers = document.querySelector('.quiz-answers .inputs');
-const submitBtn = document.getElementById('submit');
-const bulletsContainer = document.getElementById('bullets');
-const countSpan = document.querySelector('.count span');
-const results = document.querySelector('.results');
-const time = document.getElementById('time')
-let currentIdex = 0;
+let countSpan = document.querySelector('.count span')
+
+let questionsArea = document.querySelector('.quiz-area')
+
+let answersArea = document.querySelector('.quiz-answers .inputs')
+
+let bulletsContainer = document.getElementById('bullets')
+
+let allBullets;
+
+let currentIndex = 0;
+
+let theRightAnswer;
+
+let submitBtn = document.getElementById('submit')
+
 let rightAnswers = 0;
-let countInterval;
+
+let allAnswers;
+
+let results = document.querySelector('.cont')
+
+let time = document.getElementById('time')
+
+
+let timeInterval;
+
 
 function preparingData() {
+    let myRequest = new XMLHttpRequest()
+    let quizBtns = document.querySelectorAll('.quiz-btns button')
 
-    let myReq = new XMLHttpRequest();
-
-    myReq.onreadystatechange = function() {
-        if(this.readyState === 4 & this.status === 200) {
-            let questObj = JSON.parse(this.response);
-            let questCount = questObj.length;
-            
-            getQuestion(questObj[currentIdex], questCount)
-
-            handleBullets(questCount)
-            countDown(220, questCount)
-            submitBtn.addEventListener('click', function() {
-                let theRightAnswer;
-                if(currentIdex < questCount) {
-                    theRightAnswer = questObj[currentIdex].right;
-
-                }
-                checkAnswers(theRightAnswer, questCount)
-                quizArea.innerHTML = '';
-                quizAnswers.innerHTML = '';
-                
-                currentIdex++
-                controlBullets()
-                getQuestion(questObj[currentIdex], questCount)
-
-                showResults(questCount)
-                clearInterval(countInterval)
-                countDown(220, questCount)
-
-            })
-        }
-    }
+    console.log(quizBtns)
 
 
-    myReq.open('GET', 'questions.json', true);
-    myReq.send()
-}
 
-preparingData()
 
-function getQuestion(obj, count) {
-    countSpan.innerHTML = count
-    if(currentIdex < count) {
-        let questTitle = document.createElement('h2');
-        let questText = document.createTextNode(obj.title)
-        let answer;
-        questTitle.append(questText);
+    myRequest.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.response)
+            let qCount = response.length
+            console.log(response)
+            countSpan.innerHTML = qCount
+            preparingQuestion(response[currentIndex], qCount)
+            preparingBullets(qCount)
+            timerFunc(60, qCount)
 
-        quizArea.append(questTitle);
+            submitBtn.onclick = function () {
+                questionsArea.innerHTML = ``
+                answersArea.innerHTML = ``
+                currentIndex++
+                checkAnswers(theRightAnswer, allAnswers)
 
-        for(let i = 1; i <= 4; i++) {
-            let randomNum = Math.floor(Math.random() * i);
-            answer = document.createElement('div')
-            answer.className = 'answer';
-            let radio = document.createElement('input');
-            radio.type = 'radio';
-            radio.id = `answer_${i}`;
-            radio.name = `question`;
-            radio.dataset.answer = obj[`answer${i}`];
-
-            
-            if(i === 1) {
-                radio.checked = true
+                preparingQuestion(response[currentIndex], qCount)
+                handleBullets()
+                showResult(qCount)
+                clearInterval(timeInterval)
+                timerFunc(60, qCount)
             }
-            let label = document.createElement('label');
-            label.setAttribute('for', `answer_${i}`);
-
-            let labelText = document.createTextNode(obj[`answer${i}`]);
-
-            label.append(labelText)
-
-            answer.append(radio)
-            answer.append(label)
-
-            quizAnswers.append(answer)
-            
         }
-        const allAnswers = document.querySelectorAll('.answer');
-        const arrayOfAnswers = Array.from(allAnswers)
-        const arrayKeys = Array.from(Array(arrayOfAnswers.length).keys())
-        console.log(arrayKeys)
-        shuffleArr(arrayKeys)
-        console.log(arrayKeys)
 
-        allAnswers.forEach((answer, idx) => {
-            answer.style.order = arrayKeys[idx]
+    }
+
+    quizBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            console.log(btn.innerHTML)
+            myRequest.open('GET', `./${btn.innerHTML}.json`, true)
+
+            myRequest.send()
+            quizBtns.forEach(btn => btn.remove())
         })
-    }
+    })
+    // myRequest.open('GET', './questions.json', true)
 }
 
-// function handleBullets
-function handleBullets(num) {
-    for(let i = 0; i < num; i++) {
-        let bullet = document.createElement('span');
-        bullet.className = 'bullet';
+function preparingQuestion(obj, count) {
 
-        bulletsContainer.append(bullet)
-        if(i === 0) {
-            bullet.className = 'bullet on'
+
+    if (currentIndex < count) {
+        theRightAnswer = obj.right
+        let headEl = document.createElement('h2')
+        let headText = document.createTextNode(obj.title)
+        headEl.append(headText)
+        questionsArea.append(headEl)
+        console.log(obj)
+        for (let i = 0; i < 4; i++) {
+            let answerDiv = document.createElement('div')
+            let radioInput = document.createElement('input')
+            radioInput.type = 'radio'
+            radioInput.id = `answer${i + 1}`;
+            radioInput.name = 'question'
+            radioInput.dataset.answer = obj[`answer${i + 1}`]
+            let answerLabel = document.createElement('label')
+            let answerText = document.createTextNode(obj[`answer${i + 1}`])
+            answerLabel.htmlFor = `answer${i + 1}`
+            answerDiv.className = `answer`
+            answerLabel.append(answerText)
+            answerDiv.append(radioInput)
+            answerDiv.append(answerLabel)
+            answersArea.append(answerDiv)
+
+            if (i === 0) {
+                radioInput.checked = true
+            }
         }
+        allAnswers = Array.from(document.getElementsByName('question'))
+
+
     }
 }
 
-function controlBullets() {
-    let allBullets = document.querySelectorAll('.bullet')
-
-    allBullets.forEach((bullet, idx) => {
-        if(currentIdex === idx) {
+function preparingBullets(count) {
+    let bulletsDiv = document.createElement('div')
+    let cont = document.createElement('div')
+    cont.className = 'cont'
+    bulletsDiv.className = 'bullets'
+    for (let i = 0; i < count; i++) {
+        let bullet = document.createElement('span')
+        bullet.className = `bullet`
+        bulletsDiv.append(bullet)
+        cont.append(bulletsDiv)
+        bulletsContainer.append(cont)
+        if (i === 0) {
             bullet.className = `bullet on`
         }
+    }
+}
+
+function handleBullets() {
+    let allBullets = Array.from(document.querySelectorAll('.bullets span'))
+
+    allBullets.forEach((bullet, index) => {
+        if (currentIndex === index) {
+            bullet.classList.add('on')
+        }
     })
-
 }
 
-function checkAnswers(rAnswer, count) {
-    let answers = document.getElementsByName('question')
-    let theChoosenAnswer;
-    for(let i = 0; i < answers.length; i++) {
-        if(answers[i].checked) {
-            theChoosenAnswer = answers[i].dataset.answer;
+function checkAnswers(rAnswer, answerz) {
+
+    let choosenAnswer;
+    for (let i = 0; i < answerz.length; i++) {
+        if (answerz[i].checked) {
+            choosenAnswer = allAnswers[i].dataset.answer
+
         }
+
     }
-    if(theChoosenAnswer === rAnswer) {
+    if (choosenAnswer === rAnswer) {
+
         rightAnswers++
-        console.log(rightAnswers)
     }
+
 
 }
 
-function showResults(count) {
-    if(currentIdex === count) {
-        quizArea.remove()
-        quizAnswers.remove()
+
+function showResult(count) {
+    let resDiv = document.createElement('div')
+    resDiv.className = 'results'
+    if (currentIndex === count) {
+        questionsArea.remove()
+        answersArea.remove()
         submitBtn.remove()
-        if(rightAnswers > (count / 2) && rightAnswers < count) {
-            results.innerHTML = `<span class='good'>Good</span> You answered ${rightAnswers} of ${count}`
-        } else if(rightAnswers === count) {
-            results.innerHTML = `<span class='perfect'>Perfect</span> You answered ${rightAnswers} of ${count}`
+        bulletsContainer.remove()
+        if (rightAnswers > (count / 2) && rightAnswers < count) {
+
+            resDiv.innerHTML = `<span class='good'>Good </span>you answered ${rightAnswers} of ${count}`
+        } else if (rightAnswers === count && rightAnswers != 0) {
+
+            resDiv.innerHTML = `<span class='perfect'>Perfect </span>You answered ${rightAnswers} of ${count}`
         } else {
-            results.innerHTML = `<span class='bad'>Bad</span> You answered ${rightAnswers} of ${count}`
+
+            resDiv.innerHTML = `<span class='bad'>Bad </span>You answered ${rightAnswers} of ${count}`
+
         }
+        results.append(resDiv)
     }
 }
 
-function countDown(duration, count) {
-    if(currentIdex < count) {
-        let minutes = parseInt(duration / 60);
-        let seconds = parseInt(duration % 60);
-    
-        minutes = minutes < 10 ? `0${minutes}`: minutes;
-        seconds = seconds < 10 ? `0${seconds}`: seconds;
-    
-        countInterval = setInterval(() => {
-            seconds--
-            
-            
-            if(seconds < 1) {
-                minutes -= 1
-                seconds = 60
+
+function timerFunc(duration, count) {
+    let minutes = parseInt(duration / 60)
+    let seconds = parseInt(duration % 60)
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    timeInterval = setInterval(() => {
+
+        seconds--
+        if (seconds < 10) {
+            seconds = `0${seconds}`
+        }
+        if (seconds < '01' && minutes > 0) {
+            (minutes) -= 1
+            seconds = '60'
+            minutes = `0${minutes}`
+        }
+        if (seconds < '01' && minutes === '00') {
+            clearInterval(timeInterval)
+            time.innerHTML = `00:00`
+            if (currentIndex < count) {
+                submitBtn.click()
             }
-    
-            time.innerHTML = `${minutes} : ${seconds}`;
-            if(minutes == 0 && seconds === 1) {
-                clearInterval(countInterval)
-                time.innerHTML = `Time up`;
-                submitBtn.click();
-    
-            }
-    
-        }, 1000)
-    }else {
-        time.innerHTML = 'Time up ...'
-    }
 
+        }
+
+        if (currentIndex === count) {
+            clearInterval(timeInterval)
+            time.remove()
+
+        }
+
+        time.innerHTML = `${minutes} : ${seconds}`
+    }, 1000)
 }
 
 
 
+window.onload = function () {
 
-function shuffleArr(arr) {
-    let currentIdx = arr.length,
-    random ,
-    stash;
-    
-    
-    while (currentIdx > 0) {
-        random = Math.floor(Math.random() * currentIdx);
-        currentIdx--
-        
-        stash = arr[currentIdx];
-        arr[currentIdx] = arr[random];
-        arr[random] = stash;
-    }
-    return arr;
+    preparingData()
 }
+
 
 
